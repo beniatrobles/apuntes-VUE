626030dfff7e3f983bda99f8a1823cafe8b258da
beniatrobles <b.roblesberasaluce@gmail.com>
2025-05-15 18:28:19 +0200

    asdasd

--- 1final2022-2023/ejercicio2/src/views/ComprasView.vue
+++ 1final2022-2023/ejercicio2/src/views/ComprasView.vue
@@ -1,5 +1,107 @@
 <template>
-    <h1>Compras</h1>
+  <h1>Compras</h1>
+  <p>{{ mensaje }}</p>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Producto</th>
+        <th>Descripción</th>
+        <th>Precio</th>
+        <th>Stock</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="producto in productos" :key="producto.id">
+        <td>{{ producto.title }}</td>
+        <td>{{ producto.description }}</td>
+        <td>{{ producto.price }} €</td>
+        <td>{{ producto.stock }}</td>
+        <td>
+          <button @click="anadirProducto(producto)">Comprar</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <h1>Listado de Compras</h1>
+  <ul>
+    <li v-for="compra in compras" :key="compra.producto.id">
+      {{ compra.producto.title }} --- {{ compra.producto.price }} € --- Unidades: {{ compra.unidades }}
+    </li>
+  </ul>
+
+  <h2>Total de la compra: {{ total.toFixed(2) }} €</h2>
 </template>
-<script setup></script>
-<style></style>
+
+<script setup>
+import { ref, onMounted,computed } from 'vue'
+import axios from 'axios'
+
+let mensaje = ref('')
+let productos = ref([])
+let compras = ref([])
+
+
+const obtenerProductos = async () => {
+  try {
+    const response = await axios.get('/json/products.json')
+    productos.value = response.data.products
+  } catch (error) {
+    console.error("Error al obtener los productos", error)
+  }
+}
+
+const total = computed(() => {
+  let suma = 0
+  for (let i = 0; i < compras.value.length; i++) {
+    const compra = compras.value[i]
+    if (compra && compra.producto && compra.unidades) {
+      suma += compra.producto.price * compra.unidades
+    }
+  }
+  return suma
+})
+
+
+onMounted(() => {
+  const usuarioStr = sessionStorage.getItem('usuario')
+
+  if (usuarioStr) {
+    const usuario = JSON.parse(usuarioStr)
+    mensaje.value = `Realizando compra como ${usuario.name} (${usuario.email})`
+  } else {
+    mensaje.value = 'Realizando compra como invitado'
+  }
+
+  obtenerProductos()
+})
+
+// Añadir productos al carrito
+function anadirProducto(producto) {
+  let encontrado = false
+  let totalCompra = 0
+
+  for (let i = 0; i < compras.value.length; i++) {
+    if (compras.value[i].producto.id === producto.id) {
+      compras.value[i].unidades += 1
+      encontrado = true
+    //   break
+    }
+  }
+
+ 
+
+  if (!encontrado) {
+    compras.value.push({
+      producto: producto,
+      unidades: 1
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- 1final2022-2023/ejercicio2/src/views/LoginView.vue
+++ 1final2022-2023/ejercicio2/src/views/LoginView.vue
@@ -1,33 +1,71 @@
 <template>
-    <h1>Pagina de Login | Login orrialdea</h1>
-    <p>Datos de acceso</p>
-    <form action="">
-        <label for="">Email:</label>
-        <input type="text" name="" id="" placeholder="Introduce tu email"><br>
-        <label for="">Password: </label>
-        <input type="password" placeholder="Introduce tu contraseña"><br>
-        <button>Login</button>
-    </form>
+  <h1>Página de Login | Login orrialdea</h1>
+  <p>Datos de acceso</p>
+  <form @submit.prevent="login">
+    <label>Email:</label>
+    <input type="text" placeholder="Introduce tu email" v-model="usuario"><br>
+    <label>Password:</label>
+    <input type="password" placeholder="Introduce tu contraseña" v-model="contrasena"><br>
+    <button type="submit">Login usuario</button>
+  </form>
+  <button @click="entrarInvitado">Usuario Invitado</button>
 </template>
+
 <script setup>
-import axios from 'axios';
-import { ref, onMounted } from 'vue';
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 
 let usuarios = ref([])
+let usuario = ref('')
+let contrasena = ref('')
 
-let obtenerUsuarios = async () => {
-    try{
-        let response = await axios.get('/json/Usuarios.json')
-        usuarios.value = response.data
-    }catch(error){
-        console.error( "Error al obtener los usuarios", error )
+const router = useRouter()
 
-    }
+// Cargar datos desde JSON
+const obtenerUsuarios = async () => {
+  try {
+    const response = await axios.get('/json/Usuarios.json')
+    usuarios.value = response.data
+  } catch (error) {
+    console.error("Error al obtener los usuarios", error)
+  }
 }
 
-onMounted(()=>{
-     obtenerUsuarios()
-})
+// Función de login
+function login() {
+  let usuarioEncontrado = null
 
-</script> 
-<style></style>
+  for (let i = 0; i < usuarios.value.length; i++) {
+    let user = usuarios.value[i]
+    if (
+      user.email.toLowerCase() === usuario.value.trim().toLowerCase() &&
+      user.password === contrasena.value
+    ) {
+      usuarioEncontrado = user
+      break
+    }
+  }
+
+  if (usuarioEncontrado) {
+    alert(`Login exitoso: ${usuarioEncontrado.name}`)
+    // sessionStorage.setItem('usuario', usuarioEncontrado.name)
+    sessionStorage.setItem('usuario', JSON.stringify(usuarioEncontrado))
+    router.push('/compras')
+  } else {
+    alert('Email o contraseña incorrectos. Inténtalo de nuevo.')
+  }
+}
+
+// Acceso como invitado
+function entrarInvitado() {
+  sessionStorage.removeItem('usuario')
+  alert("Accediendo como invitado")
+  router.push('/compras')
+}
+
+// Al cargar la vista
+onMounted(() => {
+  obtenerUsuarios()
+})
+</script>
